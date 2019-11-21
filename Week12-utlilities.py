046eca80ab3093f2d9779e7411b1426b5cf6b89d
Jake Welch <jwelch@mymail.mines.edu>
2019-11-21 16:03:48 -0700

    5th function

--- Week12-utlilities.py
+++ Week12-utlilities.py
@@ -30,3 +30,20 @@ def FindWordCount(someList, word):
             if a.lower() == word.lower():
                 counter += 1
     PrintOutput(counter)
+
+def ScoreFinder(players, scores, name):
+    a = 0
+    true = 0
+    score = 0.0
+    output = ''
+    for i in players:
+        if i.lower() == name.lower():
+            true = 1
+            break
+        a += 1
+    if true == 1:
+        score = scores[a]
+        output = (players[a] + ' got a score of ' + str(score))
+        PrintOutput(output)
+    else:
+        PrintOutput('player not found')
