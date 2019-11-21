d5519fbc8814d4ec5a82ccf5c391ceae5574a954
Jake Welch <jwelch@mymail.mines.edu>
2019-11-21 15:36:14 -0700

    Second Function

--- Week12-utlilities.py
+++ Week12-utlilities.py
@@ -1,2 +1,10 @@
 def PrintOutput(words):
     print("OUTPUT",words)
+
+def LoadFile(fileName):
+    newList = []
+    file = open(fileName,'r')
+    lines = file.readlines()
+    for i in lines:
+        newList.append(i[0:-1])
+    return newList
